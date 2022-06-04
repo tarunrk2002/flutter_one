20badbef7d6ef127e8d1ad52a89aa9542d72a5d9
Tarun <tarunrk2002@gmail.com>
2022-06-04 19:35:12 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import "package:flutter/material.dart";
 
 void main() => runApp(
       MaterialApp(
-        home: Container(
+        home:SafeArea(
+          child:Container(
+            child: Text('hi bitch..!'),
+          width: 150,
+          height: 150,
           color: Colors.white70,
+
+        ),
+
+
+
         ),
       ),
     );
